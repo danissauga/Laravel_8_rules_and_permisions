2d38d2b36482d3b569ca65cfd35183044d2b0701
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2023-08-22 16:12:36 +0300

    Migrations

---
+++ database/migrations/2023_08_04_062200_create_settings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('purpose')->nullable();
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->unsignedBigInteger('auditor_id')->nullable();
+            $table->foreign('auditor_id')->references('id')->on('auditors');
+            $table->date('validity_date_from')->nullable();
+            $table->date('validity_date_to')->nullable();
+            $table->time('trigger')->nullable();
+            $table->string('user')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
